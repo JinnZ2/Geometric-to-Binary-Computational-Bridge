d5cb349db7ff69705e7fd76c01847c53a54f130d
JinnZ2 <surpassyourexpectations@gmail.com>
2025-10-08 08:53:19 -0500

    Update 1encode_binary.py

--- src/bridge/1encode_binary.py
+++ src/bridge/1encode_binary.py
@@ -2,3 +2,8 @@ from . import sound
 
 elif modality == "sound":
     return sound.encode_sound_gray(features, target_bits)
+
+from . import gravity
+
+elif modality == "gravity":
+    return gravity.encode_gravity_gray(features, target_bits)
